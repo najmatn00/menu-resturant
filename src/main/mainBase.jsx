9889e8b31fb2125f34f2c79ade99e860db71629f
najmatan <najmatan773@gmail.com>
2023-07-09 13:41:52 +0430

    view menu

--- src/main/mainBase.jsx
+++ src/main/mainBase.jsx
@@ -2,14 +2,19 @@ import Button from "../ui/Button";
 import Card from "../ui/Card";
 import img from "../assets/img/foodd.png";
 
+import { Link, useNavigate } from "react-router-dom";
 const MainBase = () => {
+    let navigate=useNavigate();
+    let clickHandler=()=>{
+        navigate("menupage")
+    }
     return ( <>
     <div className="flex  justify-evenly mt-4">
     <p className="Robt_bold text-6xl mt-6 ">it s not just <br/> Food, It s an<br/> Experience.</p>
     <img className="rounded-full w-80 h-80 " src={img}  />
     </div>
     <div className="ml-64 flex gap-6 absolute top-64 mt-12 z-10">
-        <Button textColor={" text-white"}>View Menu</Button>
+        <Button textColor={" text-white"} click={clickHandler}>View Menu</Button>
         <Button color={"bg-white"} textColor={" text-black"}>Book A Table </Button>
     </div>
     <div className="flex justify-evenly items-center">
