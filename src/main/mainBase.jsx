9e05db7874db26a9b880ec0efbdc75b65d51cfdd
amin75t <aminshahini18@gmail.com>
2023-07-18 12:48:44 +0330

    pc home button bug fix

--- src/main/mainBase.jsx
+++ src/main/mainBase.jsx
@@ -12,7 +12,7 @@ const MainBase = () => {
     <p className="Robt_bold text-6xl mt-6 ">it s not just <br/> Food, It s an<br/> Experience.</p>
     <img className="rounded-full hidden md:block md:w-80 md:h-80 " src={img}  />
     </div>
-    <div className="ml-64 flex gap-6 justify-center w-full absolute top-64 md:mt-12 mt-32 z-10">
+    <div className="ml-12 md:ml-64  flex gap-6  w-full absolute top-64 mt-32 z-10 ">
         <Button textColor={" text-white"} click={clickHandler}>View Menu</Button>
         <Button classCss=" border p-2 px-4 rounded-3xl bg-[#D41B27]  text-sm " color={"bg-white"} textColor={" text-black"}>Book A Table </Button>
     </div>
