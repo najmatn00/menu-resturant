9aaeb257f650e16ba92270f0a6b7c319304a1454
najmatn00 <najmatan773@gmail.com>
2023-07-24 17:24:54 +0430

    style snap

--- src/main/mainBase.jsx
+++ src/main/mainBase.jsx
@@ -12,17 +12,11 @@ const MainBase = () => {
     <p className="Robt_bold text-4xl  md:text-6xl mt-6 ml-4 md:ml-0">it s not just <br/> Food, It s an<br/> Experience.</p>
     <img className="rounded-full w-44 h-44 md:block md:w-80 md:h-80 mt-32 md:mt-2 " src={img}  />
     </div>
-    <div className="pl-12 md:pl-64  flex gap-6 mt-36 w-full absolute top-64 md:mt-32  z-10 ">
+    <div className="pl-12 md:pl-64  flex gap-6 mt-36 w-full absolute top-64 md:mt-24  z-10 ">
         <Button  textColor={" text-white"} click={clickHandler}>View Menu</Button>
         <Button classCss=" border p-2 px-4 rounded-3xl bg-[#D41B27]  text-sm " color={"bg-white"} textColor={" text-black"}>Book A Table </Button>
     </div>
-    <div className="flex pt-36  justify-evenly items-center md:pt-0 md:flex-row flex-col">
-        {/* <Card/>
-        <Card/>
-        <Card/>
-        <Card/> */}
-        
-    </div>
+    {/* <p className=" absolute left-4 top-3/4 bg-[#D41B27] text-white p-2 rounded-xl">food picture</p> */}
     <Snap></Snap>
     </>);
 
--- src/main/snap.jsx
+++ src/main/snap.jsx
@@ -1,133 +1,73 @@
 import img from "../../src/assets/img/pizza.webp"
+import img1 from "../../src/assets/img/foodd.png"
+import img2 from "../../src/assets/img/daisys_one_pot_pasta_1024x768.webp"
+
+
 const Snap = () => {
-    return (<>
-    <div className="overflow-x-auto snap-x w-full flex ">
+    return (<div className=" md:px-32 px-10">
+    <div className="overflow-x-auto snap-x w-full flex gap-8 md:mt-14 mt-20 over ">
+        
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img1} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img2} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img1} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img2} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img1} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img2} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         <div className="snap-start w-32 h-32">
            <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
-           </div>
-        </div>
-        <div className="snap-start w-32 h-32">
-           <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
-           </div>
-        </div>
-        <div className="snap-start w-32 h-32">
-           <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
-           </div>
-        </div>
-        <div className="snap-start w-32 h-32">
-           <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
-           </div>
-        </div>
-        <div className="snap-start w-32 h-32">
-           <div className="w-32 h-32">
-                 <img src={img} className="w-32 h-32" alt="" />
+                 <img src={img1} className="w-32 h-32 rounded-xl" alt="" />
            </div>
         </div>
         
         
     </div>
     
-    </>
+    </div>
       );
-
-    // return (
-    //     <div class="">
-    //     <div class=" snap-x flex gap-4 w-full overflow-x-auto">
-    //         <div class="snap-start border-4 h-fit">
-    //           <div class="w-32 bg-black h-24"></div>
-    //         </div>
-    //         <div class="snap-start border-4 h-fit">
-    //           <div class="w-32 bg-black h-24"></div>
-    //         </div>
-    //         <div class="snap-start border-4 h-fit">
-    //           <div class="w-32 bg-black h-24"></div>
-    //         </div>
-    //         <div class="snap-start border-4 h-fit">
-    //           <div class="w-32 bg-black h-24"></div>
-    //         </div>
-    //         <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //           <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //           <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //           <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //           <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //           <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //           <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //           <div class="snap-start border-4 h-fit">
-    //             <div class="w-32 bg-black h-24"></div>
-    //           </div>
-    //        </div>    
-    // </div>
-    // );
-
-
 }
  
 export default Snap;
